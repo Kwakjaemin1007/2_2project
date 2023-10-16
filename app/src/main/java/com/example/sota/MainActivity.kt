ef205c36b441031875dd1221c34ea4d570015573
kalonautica <kalonautica@gmail.com>
2023-10-17 03:50:08 +0900

    Update

--- app/src/main/java/com/example/sota/MainActivity.kt
+++ app/src/main/java/com/example/sota/MainActivity.kt
@@ -18,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+        val register = findViewById<ImageButton>(R.id.registerBtn)
+        signin.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
